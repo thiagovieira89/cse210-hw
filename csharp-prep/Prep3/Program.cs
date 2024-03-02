7d5a232807da4274f3d903e2ed92933b892f0abe
thiagovieira89 <vieira.thiagoc@gmail.com>
2024-03-02 20:53:40 -0300

    prep03 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 101);
+
+        int guess_number = -1;
+
+        while (magic_number != guess_number)
+        {
+            Console.Write("What is you guess number? ");
+            guess_number =int.Parse(Console.ReadLine());
+            
+            if (magic_number == guess_number)
+            {
+                Console.WriteLine("Congratulations!");
+            }
+            else if (guess_number < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess_number > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+
     }
 }
