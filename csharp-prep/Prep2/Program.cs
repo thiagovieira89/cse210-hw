086d6d9e80cff41bcc55b6ec71d42885884d50bd
thiagovieira89 <vieira.thiagoc@gmail.com>
2024-03-02 18:09:11 -0300

    task2 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your score: ");
+        string score = Console.ReadLine();
+        int grade = int.Parse(score);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+            
+        }
+         else if (grade >= 70)
+        {
+            letter = "C";
+            
+        }
+         else if (grade >= 60)
+        {
+            letter = "D";
+            
+        }
+         else
+        {
+            letter = "F";
+            
+        }
+        Console.WriteLine($"Your grade score is {letter}");
+    if (grade >=70)
+    {
+        Console.WriteLine("Congratulations! You passed.");
     }
+    else
+    {
+        Console.WriteLine("You will need to study more!");
+    }
+    } 
 }
