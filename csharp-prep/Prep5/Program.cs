143052cf02484c0564e23fdedfcaded7ad182757
thiagovieira89 <vieira.thiagoc@gmail.com>
2024-03-03 00:47:36 -0300

    prep05 finished

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+    void DisplayMessage()
+{
+    Console.WriteLine("Welcome to the program!");
+}
+    
+    DisplayMessage();
+    
+    string name = UserName();
+    int number = FavoriteNumber();
+    int square = SquareNumber(number);
+
+    Console.WriteLine($"Brother {name},the square of your number is {square}");
+
+
+    static string UserName ()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
     }
+    static int FavoriteNumber ()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber (int number)
+    {
+        int dubloSquare = number * number;
+        return dubloSquare;
+    }
+}
 }
