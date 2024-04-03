5554405739deae05b4736bb3d834ee027f31e843
thiagovieira89 <vieira.thiagoc@gmail.com>
2024-04-03 03:12:59 -0300

    added and finished

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Net.NetworkInformation;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius ) : base (color)
+    {
+        _radius = radius;
+    }
+   
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Rectangle s1 = new Rectangle("Blue", 5,5);
+        shapes.Add(s1);
+
+        Square s2 = new Square("black", 2);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("White", 4);
+        shapes.Add(s3);
+
+        foreach (Shape all in shapes)
+        {
+            string color = all.GetColor();
+
+            double area = all.GetArea();
+
+            Console.WriteLine($"The color is {color} and the área is {area}\n");
+
+        }
+
+        
     }
+
 }
